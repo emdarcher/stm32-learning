e67a774c2a4cfe49ac1f3f2340caa66e9a11d7b9
emdarcher <emd519@yahoo.com>
2014-07-17 13:32:19 -0700

    added some stuff

---
+++ projects/using_libopencm3/boards/SmartSensor/sensor-driver-project/main.c
@@ -0,0 +1,33 @@
+//main code
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/i2c.h>
+
+
+#include "main.h"
+
+
+
+void main(void)
+{
+    //main area
+    
+    //init the stuff
+    init_things();
+    
+    
+    //infinite loop
+    while(1){
+        
+        
+        
+    }
+    
+}
+
+void init_things(void){
+    //put various init function calls and things in here
+    
+    
+}
---
+++ projects/using_libopencm3/boards/SmartSensor/sensor-driver-project/main.h
@@ -0,0 +1,15 @@
+//main header
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/i2c.h>
+
+
+//prototypes
+void init_things(void);
+
+
+#endif
