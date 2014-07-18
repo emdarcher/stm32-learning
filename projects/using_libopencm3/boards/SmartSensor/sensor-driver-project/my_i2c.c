43cad5e923e44a4b42b2008351340d339dce5e72
emdarcher <emd519@yahoo.com>
2014-07-17 17:39:24 -0700

    added some stuff to my_i2c stuff

--- projects/using_libopencm3/boards/SmartSensor/sensor-driver-project/my_i2c.c
+++ projects/using_libopencm3/boards/SmartSensor/sensor-driver-project/my_i2c.c
@@ -5,10 +5,26 @@ void init_I2C1(void){
     //setup gpio
     init_I2C1_GPIO();
     
+    //enable afio clock
+    rcc_periph_clock_enable(RCC_AFIO);
+    
     //enable clock
     rcc_periph_clock_enable(RCC_I2C1);
     
     //setup I2C1 peripheral
+    i2c_peripheral_enable(I2C1);
+    //set duty cycle
+    i2c_set_dutycycle(I2C1, I2C_CCR_DUTY_DIV2);
+    //set own address, put to 0x00 b/c we don't really use it
+    //as we are the master here!
+    i2c_set_own_7bit_slave_address(I2C1, 0x00);
+    //enable ACK
+    i2c_enable_ack(I2C1);
+    //set 7 bit mode 
+    I2C1_OAR1 &= ~I2C_OAR1_ADDMODE;
+    //set clock speed
+    i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_8MHZ);
+    i2c_set_ccr(I2C1, 0x28);
     
     
     
