48d402074228036ffaf938f9fc55025b80c20ae3
emdarcher <emd519@yahoo.com>
2014-07-28 00:55:27 -0700

    still trying to get some trace of any output on i2c pins

--- projects/using_libopencm3/boards/SmartSensor/sensor-driver-project/my_i2c.c
+++ projects/using_libopencm3/boards/SmartSensor/sensor-driver-project/my_i2c.c
@@ -8,6 +8,10 @@ void init_I2C1(void){
     //setup gpio
     //init_I2C1_GPIO();
     
+    //setup the gpio for i2c1
+    //rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR , RCC_APB2ENR_IOPBEN);
+    
     //enable afio clock
     //rcc_periph_clock_enable(RCC_AFIO);
     rcc_peripheral_enable_clock(&RCC_APB2ENR , RCC_APB2ENR_AFIOEN);
@@ -17,34 +21,50 @@ void init_I2C1(void){
     rcc_peripheral_enable_clock(&RCC_APB1ENR , RCC_APB1ENR_I2C1EN);
     
     
+    //setup GPIO pins PB7 and PB6
+    //for af output open-drain
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, 
+                    GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN,
+                    (GPIO6 | GPIO7) );
+    
+    
     /* Disable the I2C before changing any configuration. */
-    i2c_peripheral_disable(I2C2);
+    i2c_peripheral_disable(I2C1);
 
+    //set clock speed
+    i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_8MHZ);
+    i2c_set_standard_mode(I2C1);
+    
+    i2c_set_ccr(I2C1, 0x28); //should get us a 100kHz clock
+    i2c_set_trise(I2C1, 0x09);//based on datasheet/manual
+    
+    
+    
     
     //setup I2C1 peripheral
     //i2c_peripheral_enable(I2C1);
     //set duty cycle
-    i2c_set_dutycycle(I2C1, I2C_CCR_DUTY_DIV2);
+    //i2c_set_dutycycle(I2C1, I2C_CCR_DUTY_DIV2);
     //set own address, put to 0x00 b/c we don't really use it
     //as we are the master here!
     i2c_set_own_7bit_slave_address(I2C1, 0x00);
     //enable ACK
-    //i2c_enable_ack(I2C1);
+    i2c_enable_ack(I2C1);
     //set 7 bit mode 
     //I2C1_OAR1 &= ~I2C_OAR1_ADDMODE;
     //set clock speed
-    i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_8MHZ);
-    i2c_set_ccr(I2C1, 0x28); //should get us a 100kHz clock
-    i2c_set_trise(I2C1, 0x09);//based on datasheet/manual
+    //i2c_set_clock_frequency(I2C1, I2C_CR2_FREQ_8MHZ);
+    //i2c_set_ccr(I2C1, 0x28); //should get us a 100kHz clock
+    //i2c_set_trise(I2C1, 0x09);//based on datasheet/manual
     
-    i2c_set_standard_mode(I2C1);
+    //i2c_set_standard_mode(I2C1);
     
     //setup I2C1 peripheral
     i2c_peripheral_enable(I2C1);
     
     
     //setup gpio
-    init_I2C1_GPIO();
+    //init_I2C1_GPIO();
     
     
 }
@@ -52,7 +72,8 @@ void init_I2C1(void){
 void init_I2C1_GPIO(void){
     
     //setup the gpio for i2c1
-    rcc_periph_clock_enable(RCC_GPIOB);
+    //rcc_periph_clock_enable(RCC_GPIOB);
+    rcc_peripheral_enable_clock(&RCC_APB2ENR , RCC_APB2ENR_IOPBEN);
     
     //setup GPIO pins PB7 and PB6
     //for af output open-drain
